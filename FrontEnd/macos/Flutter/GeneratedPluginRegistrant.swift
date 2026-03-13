b586bf46e413398f8e8dfeb46daf160e52a9babe
Muttayyab <muttayyab13@gmail.com>
2026-03-13 11:44:29 +0500

    Notebook Changes

--- FrontEnd/macos/Flutter/GeneratedPluginRegistrant.swift
+++ FrontEnd/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import file_picker
 import file_selector_macos
 import flutter_secure_storage_macos
 import google_sign_in_ios
-import path_provider_foundation
 import screen_retriever
 import shared_preferences_foundation
 import smart_auth
@@ -21,7 +20,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
